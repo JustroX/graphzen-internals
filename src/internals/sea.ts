79f7b05db56d1f44e5a424b8a00e213d152f35bd
JustroX <thepiesaresquared@gmail.com>
2022-01-05 16:45:26 +0800

    feat: additional functions

--- src/internals/sea.ts
+++ src/internals/sea.ts
@@ -1,42 +1,148 @@
+import {
+  createHash,
+  generateKeyPair,
+  privateDecrypt,
+  privateEncrypt,
+  publicDecrypt,
+  publicEncrypt,
+  randomBytes,
+  scrypt,
+} from "crypto";
+
 export interface Pair {
   pub: string;
   priv: string;
 }
 
 export class SEA {
-  static pair(): Pair {
-    throw new Error("Not yet implemented.");
+  /**
+   * Generates a pair of keys
+   * @returns Pair
+   */
+  static pair(): Promise<Pair> {
+    return new Promise((resolve, reject) => {
+      generateKeyPair(
+        "rsa",
+        {
+          modulusLength: 4096,
+          publicKeyEncoding: {
+            type: "spki",
+            format: "pem",
+          },
+          privateKeyEncoding: {
+            type: "pkcs8",
+            format: "pem",
+            cipher: "aes-256-cbc",
+            passphrase: process.env.PEM_PASSPHRASE,
+          },
+        },
+        (err, pub, priv) => {
+          if (err) return reject(err);
+          resolve({
+            pub,
+            priv,
+          });
+        }
+      );
+    });
   }
 
-  static encrypt(data: string, key: Pair): string {
-    throw new Error("Not yet implemented.");
+  /**
+   * Encrypts plaintext using the public key of `key`.
+   * Returns in base64 format.
+   * @param data plaintext
+   * @param key Key pair
+   */
+  static encrypt(data: string, key: Pair | string): string {
+    let pub: string;
+    if (typeof key == "string") pub = key;
+    else pub = key.pub;
+    const encrypted = publicEncrypt(pub, Buffer.from(data, "utf-8"));
+    const base64 = encrypted.toString("base64");
+    return base64;
   }
 
+  /**
+   * Decrypts an encrypted string using the private key of `key`
+   * @param data Encrypted string in base64
+   * @param key Key pair
+   */
   static decrypt(data: string, key: Pair): string {
-    throw new Error("Not yet implemented.");
+    const plain = privateDecrypt(key.priv, Buffer.from(data, "base64"));
+    const utf8 = plain.toString("utf-8");
+    return utf8;
   }
 
+  /**
+   * Cryptographically sign a string using the private key of `key`.
+   * Returns signature in base64
+   * @param data MAC or string
+   * @param key Key pair
+   */
   static sign(data: string, key: Pair): string {
-    throw new Error("Not yet implemented.");
+    const mac = privateEncrypt(key.priv, Buffer.from(data, "utf-8"));
+    const base64 = mac.toString("base64");
+    return base64;
   }
 
-  static verify(data: string, key: Pair): string {
-    throw new Error("Not yet implemented.");
+  /**
+   * Verifies a cryptographic signature from the public key of `key` or address.
+   * @param data Signature in base64
+   * @param key Key pair
+   */
+  static verify(data: string, key: Pair | string): string | false {
+    let pub: string;
+    if (typeof key == "string") pub = key;
+    else pub = key.pub;
+
+    try {
+      const mac = publicDecrypt(pub, Buffer.from(data, "base64"));
+      const utf8 = mac.toString("utf-8");
+      return utf8;
+    } catch (err) {
+      return false;
+    }
   }
 
+  /**
+   * Returns SHA-256 hash of a text in base64.
+   * @param data Plaintext
+   */
   static hash(data: string): string {
+    return createHash("sha256").update(data).digest("base64");
+  }
+
+  /**
+   * Deterministically generates a random `length`-bit string in base64
+   * @param data Phrase
+   * @param salt Phrase
+   * @param length Length in bits
+   */
+  static work(data: string, salt: string, length: number): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      scrypt(data, salt, length, (err, key) => {
+        if (err) return reject(err);
+        resolve(key.toString("base64"));
+      });
+    });
+  }
+
+  /**
+   * Encrypts `source` to `dest`
+   * @param path Path of the file
+   * @param key Key pair
+   */
+  static encryptFile(source: string, dest: string, key: Pair | string) {
+    const symmetric_key = randomBytes(16);
     throw new Error("Not yet implemented.");
   }
 
-  static work(data: string, length: number): string {
-    throw new Error("Not yet implemented.");
-  }
-
-  static encryptFile(path: string, key: Pair) {
-    throw new Error("Not yet implemented.");
-  }
-
-  static decryptFile(path: string, key: Pair) {
+  /**
+   * Decrypts a `source` to `dest`
+   * @param path Path of the file
+   * @param key Key pair
+   */
+  static decryptFile(source: string, dest: string, key: Pair) {
     throw new Error("Not yet implemented.");
   }
 }
